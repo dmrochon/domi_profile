90db342d77f676923c9c0efad34805a4cb0f60fe
Dominique Rochon <Domi@Dominiques-MacBook-Pro.local>
2019-06-26 11:43:18 -0500

    initial dump

---
+++ assets/JavaScript/best.js
@@ -0,0 +1,7 @@
+$('#friend').hover(function toggleControls() {
+    if (this.hasAttribute("controls")) {
+        this.removeAttribute("controls")
+    } else {
+        this.setAttribute("controls", "controls");
+    }
+});
